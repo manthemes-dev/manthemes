baf57590412bd61720d48cfdde1f9f112ce48685
James Alvin <jamespendon388@gmail.com>
2022-07-04 11:36:37 +0800

    [package.json]: Changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * as daisyui from "./daisyui";
 export * as material from "./material";
-export * as bootstrap from "./bootstrap";
 
 export * as moonlight from "./moonlight";
 export * as nightfox from "./nightfox";
